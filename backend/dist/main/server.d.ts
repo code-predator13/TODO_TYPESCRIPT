b2d9b06a8221f3b3b7882636542d88c76351905d
code-predator13 <monacohustler@startmail.com>
2025-11-01 11:14:44 +0300

    added sceleton

--- backend/dist/main/server.d.ts
+++
@@ -1,2 +0,0 @@
-declare function f(name: string, secondName: string): void;
-//# sourceMappingURL=server.d.ts.map
--- backend/src/main/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function f(name, secondName) {
-    console.log("Hello TypeScript!", name);
-}
-f("ZDOROVA", "BRODYAGI");
-//# sourceMappingURL=server.js.map
