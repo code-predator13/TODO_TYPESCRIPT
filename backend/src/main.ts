542194d88173146d75e0daf8a31ebd4e144354b3
code-predator13 <monacohustler@startmail.com>
2025-11-01 14:22:17 +0300

    fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');  // ← все роуты будут начинаться с /api
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env["PORT"] ?? 3001);
